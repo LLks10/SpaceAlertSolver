35346f82d24694033e4e8f5d4ea61b613e9450fd
mchl12 <12mchl.vdl@gmail.com>
2023-11-09 01:27:05 +0100

    Add CryoshieldFighter and PulseBall

---
+++ SpaceAlertSolver/ExternalCommonThreats/CryoshieldFighter.cs
@@ -0,0 +1,33 @@
+﻿namespace SpaceAlertSolver;
+
+internal partial struct Threat
+{
+    private bool HasShield { readonly get => _value1 > 0; set => _value1 = value ? 1 : 0; }
+
+    [ExternalCommonThreat("E1-06", "Cryoshield Fighter", "Cryoschild Jager")]
+    public static Threat CreateCryoshieldFighter()
+    {
+        return new Threat(4, 1, 3, 4, 2, actY: CryoshieldFighterActYZ, actZ: CryoshieldFighterActYZ, processDamage: CryoshieldProcessDamage) { HasShield = true };
+    }
+
+    public static void CryoshieldFighterActX(ref Threat @this)
+    {
+        @this.Game.DealExternalDamage(@this.Zone, 1);
+    }
+
+    private static void CryoshieldFighterActYZ(ref Threat @this)
+    {
+        @this.Game.DealExternalDamage(@this.Zone, 2);
+    }
+
+    private static void CryoshieldProcessDamage(ref Threat @this)
+    {
+        if (@this.HasShield && @this.Damage > 0)
+        {
+            @this.HasShield = false;
+            @this.Damage = 0;
+            return;
+        }
+        DefaultExternalProcessDamage(ref @this);
+    }
+}
