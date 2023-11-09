e1af4d180a665aa76ef57c38c65611b7bf5f31ef
mchl12 <12mchl.vdl@gmail.com>
2023-11-09 15:58:56 +0100

    Create Log.cs

---
+++ SpaceAlertSolver/Log.cs
@@ -0,0 +1,22 @@
+﻿namespace SpaceAlertSolver;
+
+internal class Log
+{
+    private static Log? _instance = null;
+    public static Log Instance => _instance ?? (_instance = new Log());
+
+    private readonly string _fileName;
+
+    private Log()
+    {
+        _fileName = $"Log-{DateTime.Now}";
+    }
+
+    public void WriteLine(object line)
+    {
+        string str = line.ToString() ?? "";
+        using FileStream fs = new(_fileName, FileMode.Append, FileAccess.Write);
+        using StreamWriter sw = new(fs);
+        sw.WriteLine(str);
+    }
+}
