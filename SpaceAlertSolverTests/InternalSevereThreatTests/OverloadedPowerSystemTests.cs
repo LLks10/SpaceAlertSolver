37dd9cc4246376ad259a3baa1bcbc377b247c56f
mchl12 <12mchl.vdl@gmail.com>
2023-11-16 16:32:17 +0100

    Add malfunction & kill tests | PowerSystemOverload

--- SpaceAlertSolverTests/InternalSevereThreatTests/OverloadedPowerSystemTests.cs
+++ SpaceAlertSolverTests/InternalSevereThreatTests/OverloadedPowerSystemTests.cs
@@ -33,4 +33,70 @@ public sealed class PowerSystemOverload : ThreatTestsBase
 
         Assert.IsTrue(threat.Alive);
     }
+
+    [TestMethod]
+    public void BonusDamageNotInSameTurn()
+    {
+        Threat threat = _threatPrefab;
+
+        threat.DealInternalDamage(DamageSource.RepairB, 1, -1, Position.BottomLeft);
+        threat.DealInternalDamage(DamageSource.RepairB, 1, -1, Position.BottomMiddle);
+        threat.ProcessDamage();
+        threat.DealInternalDamage(DamageSource.RepairB, 1, -1, Position.BottomRight);
+        threat.DealInternalDamage(DamageSource.RepairB, 1, -1, Position.BottomRight);
+        threat.DealInternalDamage(DamageSource.RepairB, 1, -1, Position.BottomRight);
+
+        Assert.IsTrue(threat.Alive);
+    }
+
+    [TestMethod]
+    public void MalfunctionTest()
+    {
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewThreatSpawnStep(_threatId, 3));
+        Assert.IsTrue(_defaultGame.ship.HasMalfunctionB(Position.BottomLeft));
+        Assert.IsTrue(_defaultGame.ship.HasMalfunctionB(Position.BottomMiddle));
+        Assert.IsTrue(_defaultGame.ship.HasMalfunctionB(Position.BottomRight));
+        Assert.IsFalse(_defaultGame.ship.HasMalfunctionB(Position.TopLeft));
+        Assert.IsFalse(_defaultGame.ship.HasMalfunctionB(Position.TopMiddle));
+        Assert.IsFalse(_defaultGame.ship.HasMalfunctionB(Position.TopRight));
+
+        _defaultGame.Threats[0].Alive = false;
+        _defaultGame.Threats[0].Beaten = true;
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewCleanThreatsStep());
+
+        Assert.IsFalse(_defaultGame.ship.HasMalfunctionB(Position.BottomLeft));
+        Assert.IsFalse(_defaultGame.ship.HasMalfunctionB(Position.BottomMiddle));
+        Assert.IsFalse(_defaultGame.ship.HasMalfunctionB(Position.BottomRight));
+        Assert.IsFalse(_defaultGame.ship.HasMalfunctionB(Position.TopLeft));
+        Assert.IsFalse(_defaultGame.ship.HasMalfunctionB(Position.TopMiddle));
+        Assert.IsFalse(_defaultGame.ship.HasMalfunctionB(Position.TopRight));
+    }
+
+    [TestMethod]
+    public void CanRefillInTurnOfKill()
+    {
+        _defaultGame.Init(TestUtils.CreatePlayersFromActions(ActUtils.ParseActionsFromString("bbbbbbbbbbbb")), _defaultGame.trajectories, simulationStack: new());
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewThreatSpawnStep(_threatId, 3));
+
+        // hit middle
+        _defaultGame.Players[0].TryTakeElevator();
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewPlayerActionStep(0));
+
+        // hit left
+        _defaultGame.Players[0].TryMoveLeft();
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewPlayerActionStep(0));
+
+        // hit right & kill
+        _defaultGame.Players[0].TryMoveRight();
+        _defaultGame.Players[0].TryMoveRight();
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewPlayerActionStep(0));
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewPlayerActionStep(0));
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewPlayerActionStep(0));
+
+        // refill main reactor
+        _defaultGame.Players[0].TryMoveLeft();
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewPlayerActionStep(0));
+
+        Assert.AreEqual(5, _defaultGame.ship.Reactors[1]);
+    }
 }
