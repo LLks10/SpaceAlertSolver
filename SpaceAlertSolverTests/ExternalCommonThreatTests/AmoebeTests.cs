a58a8dbe92d1529eb4d2fd62e13b94bcadeb695a
mchl12 <12mchl.vdl@gmail.com>
2023-11-16 16:01:47 +0100

    Add rocketimmune test

--- SpaceAlertSolverTests/ExternalCommonThreatTests/AmoebeTests.cs
+++ SpaceAlertSolverTests/ExternalCommonThreatTests/AmoebeTests.cs
@@ -11,7 +11,7 @@ public sealed class AmoebeTests : ThreatTestsBase
     [TestMethod]
     public void HealTest()
     {
-        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewThreatSpawnStep(_threatId, 1, true));
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewThreatSpawnStep(_threatId, 1));
         _defaultGame.PerformSingleSimulationStep(SimulationStep.NewFireCannonStep(Position.TopMiddle, true));
         _defaultGame.PerformSingleSimulationStep(SimulationStep.NewProcessDamageStep());
 
@@ -35,5 +35,26 @@ public sealed class AmoebeTests : ThreatTestsBase
         threat.ActX();
 
         Assert.AreEqual(threat.MaxHealth, threat.Health);
+    }
+
+    [TestMethod]
+    public void RocketImmuneTest()
+    {
+        _defaultGame.Init(TestUtils.CreatePlayersFromActions(ActUtils.ParseActionsFromString("c           ")), _defaultGame.trajectories, simulationStack: new());
+        var (_, gunshipId) = ThreatFactory.Instance.FindThreatMatchingName("gunship");
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewThreatSpawnStep(_threatId, 1));
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewThreatSpawnStep(gunshipId, 1));
+        _defaultGame.Threats[0].Distance = 5;
+        _defaultGame.Threats[1].Distance = 5;
+        _defaultGame.Players[0].TryMoveRight();
+        _defaultGame.Players[0].TryTakeElevator();
+
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewPlayerActionStep(0));
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewRocketUpdateStep());
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewRocketUpdateStep());
+        _defaultGame.PerformSingleSimulationStep(SimulationStep.NewProcessDamageStep());
+
+        Assert.AreEqual(_defaultGame.Threats[0].MaxHealth, _defaultGame.Threats[0].Health);
+        Assert.AreEqual(_defaultGame.Threats[1].MaxHealth - 3 + _defaultGame.Threats[1].Shield, _defaultGame.Threats[1].Health);
     }
 }
