09d7081473376cf4b0321c264178504b395b8f0d
mchl12 <12mchl.vdl@gmail.com>
2023-11-29 00:12:29 +0100

    Give cryoshield frigate its cryoshield

--- SpaceAlertSolver/ExternalSevereThreats/CryoshieldFrigate.cs
+++ SpaceAlertSolver/ExternalSevereThreats/CryoshieldFrigate.cs
@@ -5,6 +5,6 @@ public partial struct Threat
     [ExternalSevereThreat("SE1-05", "Cryoshield Frigate", "Cryoschild Fregat")]
     public static Threat CreateCryoshieldFrigate()
     {
-        return new(7, 1, 2, 8, 4, ActDealDamage(2), ActDealDamage(3), ActDealDamage(4), processDamage: CryoshieldProcessDamage);
+        return new(7, 1, 2, 8, 4, ActDealDamage(2), ActDealDamage(3), ActDealDamage(4), processDamage: CryoshieldProcessDamage) { HasShield = true };
     }
 }
