0a335fcc21b844af7e133efeac2417fd4f547d60
mchl12 <12mchl.vdl@gmail.com>
2023-11-30 17:19:37 +0100

    Implement simulation deserialization

--- SimulationGenerator/Simulation.cs
+++ SimulationGenerator/Simulation.cs
@@ -14,10 +14,32 @@ public readonly record struct Simulation(string Trajectories, ImmutableArray<str
         {
             builder.AppendLine(s);
         }
+        builder.AppendLine();
         foreach (Act[] acts in Actions)
         {
             builder.AppendLine(ActUtils.ActArrayToString(acts));
         }
         return builder.ToString();
+    }
+
+    public static Simulation Deserialize(string str)
+    {
+        string[] lines = str.Split(Environment.NewLine);
+        string trajectories = lines[0];
+        List<string> eventStrings = new();
+        int i = 1;
+        while (lines[i] != "")
+        {
+            eventStrings.Add(lines[i]);
+            i++;
+        }
+        i++;
+        List<Act[]> acts = new();
+        while (i < lines.Length)
+        {
+            acts.Add(ActUtils.ParseActionsFromString(lines[i]));
+            i++;
+        }
+        return new(trajectories, eventStrings.ToImmutableArray(), acts.ToArray());
     }
 }
